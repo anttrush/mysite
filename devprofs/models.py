fab8f776b49f8997f75c519ec635a20f440d556e
anttrush <anttrush@qq.com>
2018-11-28 00:35:23 +0800

    maybe the last edition. add gauge echart to show efficiency. change index. change developer base info. ignore coder-proj relationship, in database and in website show.

--- devprofs/models.py
+++ devprofs/models.py
@@ -5,32 +5,32 @@ class Developer(models.Model):
     dev_id = models.IntegerField()
     dev_name = models.CharField(max_length=100)
 
-    dev_score_Documentation = models.FloatField(default=0)
     dev_score_CodeStyle = models.FloatField(default=0)
-    dev_score_Performance = models.FloatField(default=0)
-    dev_score_BestPractices = models.FloatField(default=0)
-    dev_score_Security = models.FloatField(default=0)
-    dev_score_Multithreading = models.FloatField(default=0)
     dev_score_Design = models.FloatField(default=0)
+    dev_score_Documentation = models.FloatField(default=0)
     dev_score_ErrorProne = models.FloatField(default=0)
+    dev_score_Performance = models.FloatField(default=0)
+    dev_score_Multithreading = models.FloatField(default=0)
+    dev_score_Security = models.FloatField(default=0)
+    dev_score_Others = models.FloatField(default=0)
 
     # multith, net, database, IO, text, GUI, math, other
-    dev_score_API1 = models.FloatField(default=0)
-    dev_score_API2 = models.FloatField(default=0)
-    dev_score_API3 = models.FloatField(default=0)
-    dev_score_API4 = models.FloatField(default=0)
-    dev_score_API5 = models.FloatField(default=0)
-    dev_score_API6 = models.FloatField(default=0)
-    dev_score_API7 = models.FloatField(default=0)
-    dev_score_API8 = models.FloatField(default=0)
-    dev_score_API9 = models.FloatField(default=0)
-    dev_score_API10 = models.FloatField(default=0)
+    dev_score_text = models.FloatField(default=0)
+    dev_score_graph = models.FloatField(default=0)
+    dev_score_math = models.FloatField(default=0)
+    dev_score_net = models.FloatField(default=0)
+    dev_score_IO = models.FloatField(default=0)
+    dev_score_database = models.FloatField(default=0)
+    dev_score_secure = models.FloatField(default=0)
+    dev_score_other = models.FloatField(default=0)
 
-    dev_score_SolveIssue = models.IntegerField(default=0)
     dev_score_Efficient = models.FloatField(default=0)
-    # dev_score_All = models.FloatField(default=0)
 
+    dev_score_cLOC = models.IntegerField(default=0)
     dev_follower_Number = models.IntegerField(default=0)
+
+
+    # dev_score_All = models.FloatField(default=0)
     # dev_project = models.CharField(max_length=100, default="None")
 
     def __str__(self):
--- devprofs/views.py
+++ devprofs/views.py
@@ -6,7 +6,7 @@ from .models import Developer, Project
 # Create your views here.
 
 def index(request):
-    dev_list = Developer.objects.order_by('dev_id')
+    dev_list = Developer.objects.order_by('dev_follower_Number').reverse()
     proj_list = Project.objects.order_by('proj_stars')
     for proj in proj_list:
         dev = get_object_or_404(Developer, dev_id=proj.proj_owner)
@@ -15,7 +15,7 @@ def index(request):
         try:
             dev.ownproj = get_object_or_404(Project, proj_owner=dev.dev_id)
         except:
-            dev.ownproj = -1
+            dev.ownproj = None
     context = {'dev_list': dev_list, 'proj_list':proj_list}
     return render(request, 'devprofs/index.html', context)
 
@@ -25,37 +25,37 @@ def developer(request,dev_id):
     ownprojs = Project.objects.filter(proj_owner=dev_id)
 
     ca_Classlist = [
-        'Documentation', 'CodeStyle', 'Performance', 'JavaPractices', 'Security', 'Multithreading', 'Design', 'ErrorProne'
+        'Code Style','Design', 'Documentation','Error Prone', 'Performance', 'Multithreading', 'Security', 'others',
     ]
     ca_Scorelist = [
-        dev.dev_score_Documentation,
-        dev.dev_score_CodeStyle,
-        dev.dev_score_Performance,
-        dev.dev_score_BestPractices,
-        dev.dev_score_Security,
-        dev.dev_score_Multithreading,
-        dev.dev_score_Design,
-        dev.dev_score_ErrorProne
+        round(dev.dev_score_CodeStyle,3),
+        round(dev.dev_score_Design,3),
+        round(dev.dev_score_Documentation,3),
+        round(dev.dev_score_ErrorProne,3),
+        round(dev.dev_score_Performance,3),
+        round(dev.dev_score_Multithreading,3),
+        round(dev.dev_score_Security,3),
+        round(dev.dev_score_Others,3)
     ]
-    ca_Averagelist = [60,60,60,60,60,60,60,60] # need data
+    ca_Averagelist = [34.185,32.400,32.273,34.899,40.323,41.222,43.017,36.267] # need data
     ca_indicator = '['
     for caclass in ca_Classlist:
         ca_indicator += "{name: '"+caclass+"', max: 100},"
     ca_indicator += ']'
     sp_Classlist = [
-        'NetWork', 'I/O', 'Math', 'Database','Security',  'Text', 'GUI', 'Others'
+        'Text', 'Graph', 'Math', 'Net', 'IO', 'Database', 'Security', 'Others'
     ]
     sp_Scorelist = [
-        dev.dev_score_API1,
-        dev.dev_score_API2,
-        dev.dev_score_API3,
-        dev.dev_score_API4,
-        dev.dev_score_API5,
-        dev.dev_score_API6,
-        dev.dev_score_API7,
-        dev.dev_score_API8 + dev.dev_score_API9 + dev.dev_score_API10
+        round(dev.dev_score_text,3),
+        round(dev.dev_score_graph,3),
+        round(dev.dev_score_math,3),
+        round(dev.dev_score_net,3),
+        round(dev.dev_score_IO,3),
+        round(dev.dev_score_database,3),
+        round(dev.dev_score_secure,3),
+        round(dev.dev_score_other,3)
     ]
-    sp_Averagelist = [60, 60, 60, 60, 60, 60, 60, 180]
+    sp_Averagelist = [5.393, 1.575, 2.355, 8.459, 15.929, 2.072, 4.397, 34.861]
     sp_indicator = [{'name': spclass, 'max': 100} for spclass in sp_Classlist] # need data
     inProjList = [{'text':proj.proj_name, 'url':'/devprofs/project/' + str(proj.proj_id)} for proj in inprojs]
     ownProjList = [{'text':proj.proj_name, 'url':'/devprofs/project/' + str(proj.proj_id)} for proj in ownprojs]
@@ -69,7 +69,11 @@ def developer(request,dev_id):
         'ActiveIn':inProjList, # [ {'text':'', 'url':''} ]
         'Own':ownProjList, # [ {'text':'', 'url':''} ]
         'CodingAbilities':{'calist':calist, 'ca_Scorelist':ca_Scorelist, 'caAverage':ca_Averagelist},
-        'SkillPreference':{'splist':splist, 'sp_Scorelist':sp_Scorelist, 'spAverage':sp_Averagelist}}
+        'SkillPreference':{'splist':splist, 'sp_Scorelist':sp_Scorelist, 'spAverage':sp_Averagelist},
+        'effe': round(dev.dev_score_Efficient,2),
+        'cLOC': dev.dev_score_cLOC,
+        'followerNum': dev.dev_follower_Number
+    }
 
     return render(request, 'devprofs/developer.html', {'dict_for_html':dict_for_html})
 
